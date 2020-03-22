253aed5e59edda66ef59c69cd756fd398f0590e2
Tareq Kirresh <kirreshtareq@gmail.com>
2020-03-22 14:35:17 +0200

    refactored to use redom-router

---
+++ src/main/webapp/src/app.js
@@ -0,0 +1,46 @@
+import { router, mount, el, text} from 'redom';
+import './styles/app.css';
+import './styles/bootstrap.min.css';
+import {App, goto} from 'redom-router';
+class Home {
+    constructor() {
+        this.el = el("h1", "home");
+    }
+
+    update(data) {
+        console.log(data);
+    }
+}
+
+class Admin {
+    constructor() {
+        this.el = el("h1", "admin");
+    }
+
+    update(data) {
+        console.log(data);
+    }
+}
+
+class AdminMiddleware {
+    constructor() {
+
+    }
+
+    exec(currentView, nextView, params) {
+        if (nextView == 'admin') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+
+const app = new App().routes({
+    home: Home,
+    default: Home,
+    admin: Admin
+}).middlewares([new AdminMiddleware()]).start()
+
